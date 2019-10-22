e6db6e83a07312147de2c3cb50d2374da94f4463
Mayur Parmar <mrparmar>
2019-10-22 16:16:29 +0530

    Initial Commit

---
+++ Registration form/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Registration form
+//
+//  Created by Agstya Technologies on 22/10/19.
+//  Copyright © 2019 Mayur. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
